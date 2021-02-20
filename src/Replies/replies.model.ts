c235583d08fcd5506bd96bbb584cbab5200550a7
Jenia <yevgeniyg10@gmail.com>
2021-02-20 17:18:44 +0200

    added replies model

---
+++ src/Replies/replies.model.ts
@@ -0,0 +1,43 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+
+import { ApiProperty, ApiResponseProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+import * as mongoose from "mongoose";
+
+import { BasicEntityDto } from "../common/basicEntity.dto";
+import { Post } from "src/Posts/posts.model";
+import { User } from "src/users/user.model";
+import { Challenge } from "src/challenges/challenge.model";
+
+export class ReplyDto extends BasicEntityDto {
+  @ApiResponseProperty()
+  _id: string;
+
+  @ApiProperty()
+  @IsString()
+  createdBy: mongoose.ObjectId;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  video: string;
+
+  @ApiProperty()
+  replierId: mongoose.ObjectId;
+}
+
+@Schema()
+export class Reply extends mongoose.Document {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "Challenge" })
+  challengeId: Challenge;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "Post" })
+  postId: Post;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
+  replierId: User;
+}
+
+export const ReplySchema = SchemaFactory.createForClass(Reply);
