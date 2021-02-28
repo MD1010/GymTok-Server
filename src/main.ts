dc8ff335e703631bf6a47444fa250ef91381ed9c
Jenia <yevgeniyg10@gmail.com>
2021-02-28 21:10:02 +0200

    removed unnecessary comments

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,6 @@ import { AllExceptionsFilter } from "./common/allExceptionsFilter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const adminConfig: ServiceAccount = {
-  //   projectId: process.env.FIREBASE_PROJECT_ID,
-  //   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  //   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  // };
-  // admin.initializeApp({
-  //   credential: admin.credential.cert(adminConfig),
-  //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  // });
   app.enableCors();
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe());
